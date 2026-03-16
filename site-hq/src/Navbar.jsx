2c4cd042e82cd01ea8ee0dcff79e51c7b8d9e7ce
lyre <herculesjose523@gmail.com>
2026-03-16 19:24:37 -0300

    Primeiro commit

---
+++ site-hq/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>HQs</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>HQs</li>
+        <li>Favoritos</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
