a4a861642a5cb66fb4a033731ac9d485368ae4e2
lyre <herculesjose523@gmail.com>
2026-03-16 23:10:26 -0300

    Adiciona textoFrame com 5 <p> posicionados individualmente sobre frameTexto

--- site-hq/src/App.jsx
+++ site-hq/src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import "./index.css";
+import pow from "../img/POW!.png";
 
 function App() {
   return (
@@ -9,7 +10,35 @@ function App() {
       {/* 1ª Seção: Homem Aranha (Topo) */}
       <section className="section-hero">
         <div className="content">
-          {/* Conteúdo do Frame 2 */}
+       <div className="frames">
+
+      <img src="../img/POW!.png" className="pow" />
+
+      <p className="textoPow">Lorem ipsum dolor sit amet.</p>
+      
+      <img src="../img/AvenFundo01.png" className="avenFundo" alt="fundo" />
+
+  <div className="textoAven">
+    <p>Lorem</p>
+    <p>Lorem</p>
+    <p>Lorem</p>
+  </div>
+
+      <img src="../img/PrimeiraLinhaDoFrame01.png" className="linha1" />
+
+       <img src="../img/frameDeTextos01.png" className="frameTexto" />
+ <div className="textoFrame">
+    <p className="p1">Lorem ipsum dolor. </p>
+    <p className="p2">Lorem ipsum  sit amet,  adipiscing elit. </p>
+    <p className="p3">Lorem ipsum dolor sit amet, consectetur  .</p>
+    <p className="p4">Lorem ipsum dolor sit amet,. </p>
+    <p className="p5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+
+      <img src="../img/SegundaLinhaDoFrame01.png" className="linha2" />
+
+    </div>
+
         </div>
       </section>
 
